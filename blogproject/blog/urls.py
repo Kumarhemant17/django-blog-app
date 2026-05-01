0fd324acfd94bc4755d0b2d21ff956d2fb595e31
Hemant Kumar <kumar.hemant1797@gmail.com>
2026-05-01 14:28:57 +0530

    Stage 3: Auth system, create post, conditional UI

--- blogproject/blog/urls.py
+++ blogproject/blog/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('post/<int:id>/', views.post_detail, name='detail'),
+    path('create/', views.create_post, name='create'),
 ]
--- blogproject/blog/views.py
+++ blogproject/blog/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
-from django.shortcuts import get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Post
+from .forms import PostForm
+from django.contrib.auth.decorators import login_required
 
 def home(request):
     posts = Post.objects.all().order_by('-created_at')
@@ -9,3 +10,17 @@ def home(request):
 def post_detail(request, id):
     post = get_object_or_404(Post, id=id)
     return render(request, 'detail.html', {'post': post})
+
+@login_required
+def create_post(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user   # VERY IMPORTANT
+            post.save()
+            return redirect('home')
+    else:
+        form = PostForm()
+
+    return render(request, 'create.html', {'form': form})
