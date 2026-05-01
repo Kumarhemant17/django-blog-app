6646ff15e2076c4fea044e6106ed6a7b09693f25
Hemant Kumar <kumar.hemant1797@gmail.com>
2026-05-01 11:33:53 +0530

    Stage 2: Display posts, detail page, static files setup

--- blogproject/blog/views.py
+++ blogproject/blog/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from .models import Post
 
-# Create your views here.
+def home(request):
+    posts = Post.objects.all().order_by('-created_at')
+    return render(request, 'home.html', {'posts': posts})
+
+def post_detail(request, id):
+    post = get_object_or_404(Post, id=id)
+    return render(request, 'detail.html', {'post': post})
